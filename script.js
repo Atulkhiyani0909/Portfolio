49e71fc73a0e0a431074e899f0e4d81677102f16
Atul <atulkhiyani09@gmail.com>
2024-10-01 16:16:10 +0530

    updated the error

---
+++ script.js
@@ -0,0 +1,19 @@
+const clicking=document.getElementById('menu-icon');
+const navbar=document.querySelector('.navbar');
+
+clicking.addEventListener('click',()=>{
+    navbar.classList.toggle('active');
+});
+
+// Get all the navigation links
+const navLinks = document.querySelectorAll('.navbar a');
+
+// Add click event listener to each navigation link
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        // Remove the 'active' class from the navbar when a link is clicked
+        navbar.classList.remove('active');
+    });
+});
+
+
